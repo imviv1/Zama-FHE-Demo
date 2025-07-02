e223b53009028a9172d3f5c98e770823d909ab5a
imviv1 <vk572487@gmail.com>
2025-07-03 01:41:08 +0530

    Added decrypt function to fhe_demo.py

--- fhe_demo.py
+++ fhe_demo.py
@@ -2,3 +2,11 @@
 def encrypt(data):
     return data + 10  # Dummy encryption
 print("Demo setup complete!")
+# Simple FHE-like demo
+def encrypt(data):
+    return data + 10  # Dummy encryption
+
+def decrypt(encrypted_data):
+    return encrypted_data - 10  # Dummy decryption
+
+print("Demo setup complete!")
